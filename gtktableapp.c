d27d744eb580064d04a5bdecc3d1ba7d12097291
Ravindra Wagh <raviwagh@cyberdude.com>
2015-09-04 13:21:49 +0530

    Adding button and table to application

--- gtktableapp.c
+++ gtktableapp.c
@@ -4,7 +4,8 @@
 int main (int argc, char *args[])
 {
     /* Declare the Gtkwidget window to show main window*/
-    GtkWidget *window;
+    GtkWidget *window, *table, *button;
+
     /*This below statement initializes all stuff needed to GTK and can parse
     command line arguments for the GTK application. */
     gtk_init(&argc, &args);
@@ -13,11 +14,23 @@ int main (int argc, char *args[])
     */
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
+    /* Initialization of table, with 3 columns, 3 rows and with TRUE homogenous
+    property */
+    table = gtk_table_new (3, 3, TRUE);
+    button = gtk_button_new_with_label("Button");  // Button with label
+
+    /* This function below attaches button to table with value according top,
+    down, left and right values. */
+    gtk_table_attach_defaults (GTK_TABLE(table), button, 0, 1, 0, 1);
+
+
     /* This is function sends the exit signal to the program when Close [X]
     button is pressed. */
     g_signal_connect_swapped(G_OBJECT(window), "destroy",
                                 G_CALLBACK(gtk_main_quit), NULL);
 
+    /* Add a widget to GTK container, which is in this case is window */
+    gtk_container_add(GTK_CONTAINER(window), table);
     /* Function to show the widget. */
     gtk_widget_show_all(window);
 
